9b7e8c2ba6bdddf1e386b95ddd0f4940b246b67d
kenzoo08 <112526341+kenzoo08@users.noreply.github.com>
2022-09-02 14:24:16 +0700

    Add files via upload

---
+++ python4-tkj4.py
@@ -0,0 +1,48 @@
+# rumus persegi sisi * sisi
+
+sisi1 = 20
+sisi2 = 20
+luas_p = sisi1 * sisi2
+print("luas persegi adalah", luas_p)
+
+
+
+# rumus persegi panjang * lebar
+
+panjang = 10
+lebar = 20
+luas_pp = panjang * lebar
+
+print("luas persegi panjang adalah", luas_pp)
+
+
+# rumus trapesium 1/2 * (a+b) * tinggi
+print("\nTrasium")
+a = 6
+b = 10
+t = 8
+luas_t = (a+b) * t / 2
+print("luas trasium adalah", luas_t)
+
+# rumus segitiga 1/2 * a * t
+print("\nSegitiga")
+a = 4
+t = 10
+luas_L = a * t * 2
+print("luas segitiga adalah", luas_L)
+
+
+# rumus jajargenjang K = 2 * (a+b)
+a = 6
+b = 10
+k = 2
+luas_k = (a+b) * k
+print("luas jajargenjang adalah", luas_k)
+
+
+
+# rumus lingkaran 3,15 * r
+print("\nLingkaran")
+r = 300
+luas_l = 3,15 * r
+print("luas lingkaran" , luas_l)
