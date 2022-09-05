d25affadad7fc350981be0a8594b2f92f974cfcc
kenzoo08 <112526341+kenzoo08@users.noreply.github.com>
2022-09-05 10:00:45 +0700

    Add files via upload

---
+++ python6-tkj4.py
@@ -0,0 +1,27 @@
+# list yang berisi kumpulan integer
+list_nilai = [80, 100, 100, 70]
+
+# list campuran berbagai tipe data
+list_jawaban = [150, 66.77, 'Presiden Sukarno', False]
+
+print("keysa alvino k")
+
+
+print('mangga')
+print('pisang')
+print('rambutan')
+print('anggur')
+
+print('melon[-1]')
+print('nanas[-2]')
+print('alpuket[-3]')
+print('semangka[-4]')
+
+print('pisang[0:1]')
+print('melon[0:2]')
+print('nanas[1:3]')
+print('anggur[0:-1]')
+print('belimbing[-1:-3]')
+print('mangga[-1:3]')
+print('semangka[-3:-1]')
+
