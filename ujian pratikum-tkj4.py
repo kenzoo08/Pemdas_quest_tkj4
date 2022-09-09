b5b6c8c181224f517f3d25e82585c3b620ddda3d
kenzoo08 <112526341+kenzoo08@users.noreply.github.com>
2022-09-09 14:53:50 +0700

    Add files via upload

---
+++ ujian pratikum-tkj4.py
@@ -0,0 +1,33 @@
+#rumus persegi panjang * lebar
+
+panjang = 10
+lebar = 20
+luas_pp = panjang * lebar
+print("luas persegi panjang adalah", luas_pp)
+
+#rumus persegi sisi * sisi
+
+sisi1 = 20
+sisi2 = 20
+luas_p = sisi1 * sisi2
+print("luas persegi adalah", luas_p)
+
+# rumus tabung 1/3 * r * r * tinggi
+print("\ntabung")
+r1 = 4
+r2 = 10
+tinggi = 4
+luas_t = r2 * r1 * tinggi
+print("luas tabung adalah", luas_t)
+
+# rumus limas 22/7 * l * t
+print("\nLimas")
+l = 4
+t = 10
+luas_L = l * t * 2
+print("luas limas adalah", luas_L)
+
+# rumus celcius 4/5
+celcius = 1000
+r = 11
+print("celcius adalah", celcius)
